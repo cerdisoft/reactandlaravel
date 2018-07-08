38cc5aae4207bfded0ed799d77490fa944121477
Humberto <1201hs@gmail.com>
2018-07-08 14:44:52 -0500

    cambios

---
+++ resources/assets/js/components/App.js
@@ -0,0 +1,82 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: '',
+            tasks: []
+        }
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.renderTask = this.renderTask.bind(this);
+    }
+    //handle change
+    handleChange(e) {
+        this.setState({
+            name: e.target.value
+        });
+        //console.log(e.target.value);
+    }
+    //handle submit
+    handleSubmit(e) {
+        e.preventDefault();
+        axios.post('/tasks', {
+            name: this.state.name
+        }).then(response => {
+            //console.log('from handle sumit', response);
+            this.setState({
+                tasks: [response.data, ...this.state.tasks],
+                name: ''
+            })
+        });
+    }
+    //render task
+    renderTask() {
+        return this.state.tasks.map(task => (
+            <div key={task.id} className="media">
+                <div className="media-body">
+                    <div>
+                        {task.name}
+                    </div>
+                </div>
+            </div>            
+        ));
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <div className="row justify-content-center">
+                    <div className="col-md-8">
+                        <div className="card">
+                            <div className="card-header">React Component</div>
+
+                            <div className="card-body">
+                                <form onSubmit={this.handleSubmit}>
+                                    <div className="form-group">
+                                        <textarea 
+                                            onChange={this.handleChange}
+                                            value={this.state.name}
+                                            className="form-control" 
+                                            rows="5" 
+                                            maxLength="255"
+                                            placeholder="Create a new task" 
+                                            required 
+                                        />
+                                    </div>
+                                    <button type="submit" className="btn btn-primary">Create Task</button>
+                                </form>
+                                <hr />
+
+                                {this.renderTask()}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default App;
